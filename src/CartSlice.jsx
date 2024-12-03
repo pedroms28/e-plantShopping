f26ad3e55b3cacf89e051186571d7d1433c9b796
pedroms9428@gmail.com <pedroms9428@gmail.com>
2024-12-04 00:14:51 +0100

    2-State management using Redux

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,28 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const foundItem = state.items.find((item) => item.name === name);
+      if (!foundItem) {
+        state.items.push({
+          name: name,
+          image: image,
+          cost: cost,
+          quantity: 1
+        });
+      } else {
+        foundItem.quantity++;
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter((item) => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const foundItem = state.items.find((item) => item.name === name);
+      if (foundItem) {
+        foundItem.quantity = quantity;
+      }
     },
   },
 });
